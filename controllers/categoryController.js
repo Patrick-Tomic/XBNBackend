181052def1cc3ceb12932f7db92f705716d95057
Patrick-Tomic <pat12tomic@gmail.com>
2024-03-26 12:11:13 -0400

    routes for brands and categories

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,12 +1,13 @@
 const {body, validationResult} = require('express-validator')
+const User = require('../models/userModel')
 const Item = require('../models/itemModel')
 const Brand = require('../models/brandModel')
 const Category = require('../models/categoryModel')
 
-exports.brands = async(req,res,next) => {
+exports.categories = async(req,res,next) => {
     try{
-        const allBrands = await Brand.find({},'name').sort({name:1}).exec()
-        res.status(200).json({brands:allBrands})
+        const allCateg = await Category.find({}, 'type').sort({type:1}).exec()
+        res.status(200).json({categories:allCateg})
     }catch(err){
         res.status(403).json({err})
     }
--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,14 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/userController')
 const itemController = require('../controllers/itemController')
-
+const brandController = require('../controllers/brandController')
+const categoryController = require('../controllers/categoryController')
 router.post('/signup', userController.signup)
 
 router.get('/allitems', itemController.items)
+
+router.get('/brands',brandController.brands)
+
+router.get('/categories', categoryController.categories)
 
 module.exports = router
